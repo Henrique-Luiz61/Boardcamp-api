426e7c30f876ff5551c49a1b582a4980a1c39cad
Henrique-Luiz61 <henriqueluiz266@gmail.com>
2023-09-18 23:27:51 -0300

    projeto estruturado

---
+++ src/database/database.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import pg from "pg";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.NODE === "production") configDatabase.ssl = true;
+
+export const db = new Pool(configDatabase);
+
+// Verificar a conexão com o banco de dados
+db.connect()
+  .then(() => {
+    console.log("Conexão bem-sucedida com o banco de dados!");
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao banco de dados:", err);
+  });
