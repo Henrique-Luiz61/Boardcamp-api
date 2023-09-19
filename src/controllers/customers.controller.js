30732e206e343bd76c17f4328215ebb4ff44b927
Henrique-Luiz61 <henriqueluiz266@gmail.com>
2023-09-19 12:22:59 -0300

    post customers

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,7 +1,26 @@
 import {
+  createCustomerDB,
+  getCustomerByCpfDB,
   getCustomerByIdDB,
   getCustomersDB,
 } from "../repositories/customers.repository.js";
+
+export async function postCustomer(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
+
+  try {
+    const conflictCpf = await getCustomerByCpfDB(cpf);
+
+    if (conflictCpf.rowCount > 0)
+      return res.status(409).send({ message: "Already registered CPF!" });
+
+    await createCustomerDB(name, phone, cpf, birthday);
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
 
 export async function getCustomers(req, res) {
   try {
@@ -25,7 +44,7 @@ export async function getCustomerById(req, res) {
     if (customer.rowCount === 0)
       return res.status(404).send({ message: "Customer not found!" });
 
-    res.send(customer.rows);
+    res.send(customer.rows[0]);
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/repositories/customers.repository.js
+++ src/repositories/customers.repository.js
@@ -1,5 +1,19 @@
 import { db } from "../database/db.connection.js";
 
+export function createCustomerDB(name, phone, cpf, birthday) {
+  return db.query(
+    `
+    INSERT INTO customers (name, phone, cpf, birthday)
+      VALUES ($1, $2, $3, $4);
+    `,
+    [name, phone, cpf, birthday]
+  );
+}
+
+export function getCustomerByCpfDB(cpf) {
+  return db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf]);
+}
+
 export function getCustomerByIdDB(userId) {
   return db.query(`SELECT * FROM customers WHERE id = $1;`, [userId]);
 }
